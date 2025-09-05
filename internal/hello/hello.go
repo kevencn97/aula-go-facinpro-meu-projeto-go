9d81f82272f6340e3dce9e58652b082853b04bc5
Keven Costa Negreiros <keven-costa@hotmail.com>
2025-09-05 19:50:57 -0300

    Mensagem personalizada alterada

--- internal/hello/hello.go
+++ internal/hello/hello.go
@@ -1,12 +1,12 @@
 // Pacote interno (internal)
 // Convenção de mercado: colocar lógicas que não serão exportadas para outros projetos
-package hello  
+package hello
 
 // Importa o pacote fmt para formatação de strings e impressão
 import "fmt"
 
 // Função simples para demonstrar organização do código
 func SayHello() {
-    // Imprime uma mensagem no console
-    fmt.Println("Olá, mundo! FacINpro! 👋")
+	// Imprime uma mensagem no console
+	fmt.Println("Olá, bem vindo a aula de golang 👋")
 }
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/seu-usuario/meu-projeto-go/internal/hello"
+
+func main() {
+	hello.SayHello()
+}
