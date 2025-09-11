4c039cd456ed197087e87e5f227e8845bab27831
Keven Costa Negreiros <keven-costa@hotmail.com>
2025-09-11 18:01:18 -0300

    Função Fibonacci e MAIN atualizada, tanto com Hello modificado quanto c/ fibonacci adicionado

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,6 +4,7 @@ package main
 
 // Importa os pacotes necessários
 import (
+	"aulago/internal/fibonacci"
 	"aulago/internal/hello"
 	"fmt"
 )
@@ -12,4 +13,14 @@ import (
 func main() {
 	fmt.Println("🚀 Meu primeiro projeto em Go com estrutura de mercado!")
 	hello.SayHello()
+	fmt.Println("----")
+	// --- Parte 2: Fibonacci ---
+	// Define qual número da sequência queremos
+	posicao := 10
+
+	// Chama a função Fibonacci do nosso pacote e guarda o resultado
+	resultadoFib := fibonacci.Fibonacci(posicao)
+
+	// Imprime o resultado formatado
+	fmt.Printf("O %dº número na sequência de Fibonacci é: %d\n", posicao, resultadoFib)
 }
